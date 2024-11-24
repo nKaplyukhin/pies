f8203c592cba52d16e306da75b9e4a52147f8ee3
nKaplyukhin <nikita-140294@mail.ru>
2024-11-24 20:52:32 +0700

    add swiper

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import Swiper from "swiper";
 import "./styles/styles.scss";
+import { Autoplay } from "swiper/modules";
+
+new Swiper(".collection__swiper", {
+  modules: [Autoplay],
+  loop: true,
+  spaceBetween: 16,
+  slidesPerView: 5,
+
+  autoplay: {
+    delay: 3000,
+  },
+});
