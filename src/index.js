cc2ecf50e8881cb9a451e285d7ea55849c49d6b5
nKaplyukhin <nikita-140294@mail.ru>
2024-12-08 20:07:30 +0700

    add burger menu

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import Swiper from "swiper";
 import "./styles/styles.scss";
 import { Autoplay } from "swiper/modules";
 
+
+const BURGER_BUTTTON_CLASS = "burger__button"
+const ACTIVE_BURGER_BUTTTON_CLASS = "burger__button_active"
+
+const BURGER_MENU_CLASS = "burger-menu"
+const ACTIVE_BURGER_MENU_CLASS = "burger-menu_active"
+
+const classSelector = (className) => document.querySelector(`.${className}`)
+
 new Swiper(".collection__swiper", {
   modules: [Autoplay],
   loop: true,
@@ -20,3 +29,18 @@ new Swiper(".collection__swiper", {
     delay: 3000,
   },
 });
+
+const burgerButton = classSelector(BURGER_BUTTTON_CLASS)
+const burgerMenu = classSelector(BURGER_MENU_CLASS)
+
+
+burgerButton.addEventListener("click", (e) => {
+  if (burgerButton.classList.contains(ACTIVE_BURGER_BUTTTON_CLASS)) {
+    burgerButton.classList.remove(ACTIVE_BURGER_BUTTTON_CLASS)
+    burgerMenu.classList.remove(ACTIVE_BURGER_MENU_CLASS)
+    return
+  }
+
+  burgerButton.classList.add(ACTIVE_BURGER_BUTTTON_CLASS)
+  burgerMenu.classList.add(ACTIVE_BURGER_MENU_CLASS)
+})
